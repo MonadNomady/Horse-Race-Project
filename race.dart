1d4006776f3e737a6fb2aecadb2897bf6b667e58
Madison Perez <116519670+MonadNomady@users.noreply.github.com>
2026-02-12 23:21:39 -0600

    updated race.dart

--- race.dart
+++ race.dart
@@ -2,19 +2,56 @@ import 'horse.dart';
 import 'dart:math';
 
 class Race {
-  // Store a list of horses and race distance
+  List<Horse> horses;
+  int raceDistance;
   
-  // Constructor
+  Race(this.horses, this.raceDistance);
 
   List<Horse> runRace() {
-    // 1. Refresh horse stats (Stamina to Max, Distance/Speed to 0)
+
+    //refresh stats
+    for (var horse in horses) {
+      horse.currentStamina = horse.maxStamina;
+      horse.currentDistance = 0;
+      horse.currentSpeed = 0;
+    }
+
+    List<Horse> winners = [];
+    var random = Random();
     
-    // 2. Start round-by-round loop
-    // TODO: Apply movement rules (Stamina, Acceleration, Luck) 
-    
-    // 3. Check for winners
-    
-    // 4. Return top 3
-    return []; 
+    // play rounds until 3 horses cross
+    while (winners.length < 3) {
+      for (var horse in horses) {
+
+        if (winners.contains(horse)) continue; //skip already finished horses
+
+        horse.currentStamina--; //decrease stamina by 1
+
+        //if stamina not 0 then accelerate
+        if (horse.currentStamina >= 0) {
+          horse.currentSpeed += horse.acceleration;
+        }
+
+        //cap speed to max
+        if (horse.currentSpeed > horse.maxSpeed) {
+          horse.currentSpeed = horse.maxSpeed.toDouble();
+        }
+
+        int roll = random.nextInt(11); //luck check
+      
+        if (roll <= horse.luck) {
+          horse.currentDistance += horse.currentSpeed; //if doesnt stumble
+        }
+
+        //check for finish line
+        if (horse.currentDistance >= raceDistance) {
+          winners.add(horse);
+        }
+
+      }
+    }
+
+    return winners;
+
   }
 }
