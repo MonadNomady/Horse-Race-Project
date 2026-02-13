5c95b3c24b029120533b97f3c3bceee92a599b8f
Shyana Sandhu <shyanas2003@gmail.com>
2026-02-12 23:19:01 -0600

    Horse.dart completed

--- horse.dart
+++ horse.dart
@@ -13,7 +13,7 @@ class Horse {
   late int luck;
   late String name;
 
-  Horse() {
+  Horse(List<String> nouns, List<String> adj) {
     var random = Random();
 
     maxSpeed = random.nextInt(91) + 10;
@@ -23,11 +23,46 @@ class Horse {
 
     currentStamina = maxStamina;
 
-    
-    // TODO: Read from nouns.txt and adj.txt for the name
+    int temp = random.nextInt(2);
+
+    if(temp == 0) {
+      name = nouns[random.nextInt(nouns.length)];
+
+    }else {
+      String rAdj = adj[random.nextInt(adj.length)];
+      String rNoun = nouns[random.nextInt(nouns.length)];
+
+      name = rAdj + " " + rNoun;
+    }
+
   }
 
   void printHorseInfo() {
     print("Name: $name | Max Speed: $maxSpeed | Max Stamina: $maxStamina | Accel: $acceleration | Luck: $luck");
   }
 }
+
+// TODO: Read from nouns.txt and adj.txt for the name
+    void main() async {
+      List<String> nContents = [];
+      List<String> aContents = [];
+
+      final nFile = File('nouns.txt');
+      final aFile = File('adj.txt');
+
+      if(await nFile.exists()){
+        nContents = await nFile.readAsLines();
+      } else {
+        print('File not found');
+      }
+
+      if(await aFile.exists()){
+        aContents = await aFile.readAsLines();
+      } else {
+        print('File not found');
+      }
+
+      Horse horse = Horse(nContents, aContents);
+      horse.printHorseInfo();
+
+    }
