086759cdb78d156ad5d96a4f3a233b0f7418e08e
Madison Perez <116519670+MonadNomady@users.noreply.github.com>
2026-02-12 23:42:03 -0600

    updated app.dart

--- app.dart
+++ app.dart
@@ -3,10 +3,23 @@ import 'horse.dart';
 import 'race.dart';
 
 void main() {
-  // 1. Query user for: distance, horses per race, and number of races 
+  //prompt user for: distance, horses per race, and number of races 
+  stdout.write('Enter the distance of the race: ');
+  String? raceDistanceInput = stdin.readLineSync();
+
+  stdout.write('Enter the horses per race: ');
+  String? horseCountInput = stdin.readLineSync();
+
+  stdout.write('Enter the number of races: ');
+  String? numRacesInput = stdin.readLineSync();
+
+  int raceDistance = int.parse(raceDistanceInput!);
+  int horseCount = int.parse(horseCountInput!);
+  int numRaces = int.parse(numRacesInput!);
   
-  // 2. Create initial list of random horses
-  
+  //create list of random horses
+  List<Horse> participants = List.generate(horseCount, (index) => Horse());
+
   // 3. Loop through the number of races requested 
   //    - Create the Race 
   //    - Get winners and print their stats
